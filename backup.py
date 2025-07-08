8dabf05373019d232e36e8c1c54e4a1f09680ba6
Arnav Jain <git@arnav.jain.se>
2025-07-08 10:23:42 +0200

    print messages on which version is being used for run

--- backup.py
+++ backup.py
@@ -146,10 +146,12 @@ class CommandRunner:
                     continue
 
             if os.path.exists(f'dce/DiscordChatExporter.Cli.exe'):
+                print("Downloading using DiscordChatExporter.Cli")
                 dce_path = '"dce/DiscordChatExporter.Cli"'
                 common_args = f'--format Json --media --reuse-media --fuck-russia --markdown false'
                 custom_args = f'--token "{guild["tokenValue"]}" --media-dir "exports/{guild["guildName"]}/_media/" --output "exports/{guild["guildName"]}/{nowTimestampFolder}/"'
             elif is_linux() and shutil.which('docker') is not None:
+                print(f"Downloading using Docker version {DOCKER_IMAGE}")
                 dce_path = f'docker run --rm -i -v "$(pwd)/exports/{guild["guildName"]}/_media:/out/{guild["guildName"]}/_media" -v "$(pwd)/exports/{guild["guildName"]}/{nowTimestampFolder}:/out/{guild["guildName"]}/{nowTimestampFolder}" {DOCKER_IMAGE}'
                 common_args = f'--format Json --media --reuse-media --fuck-russia --markdown false'
                 custom_args = f'--token "{guild["tokenValue"]}" --media-dir "{guild["guildName"]}/_media/" --output "{guild["guildName"]}/{nowTimestampFolder}/"'
